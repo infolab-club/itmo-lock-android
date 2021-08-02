b54790f480eed252a525cee8ac0e06827c334c4b
Ivan Kostylev <jkostylev47@gmail.com>
2021-08-02 14:23:14 +0300

    Init empty project.

---
+++ app/src/main/java/club/infolab/itmo_lock/MainActivity.kt
@@ -0,0 +1,18 @@
+package club.infolab.itmo_lock
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import club.infolab.itmo_lock.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/club/infolab/itmo_lock/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package club.infolab.itmo_lock.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
